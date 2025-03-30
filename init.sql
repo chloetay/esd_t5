6dde0eddc900929434e1dc15041b83f2c725bbc4
jovibong <jovi.bong.2023@scis.smu.edu.sg>
2025-03-30 18:18:15 +0800

    small error on initsql

--- init.sql
+++ init.sql
@@ -26,9 +26,11 @@ CREATE TABLE COURSE (
 INSERT INTO COURSES (courseId, courseName, courseDescription) VALUES
 ("course_00001", "Introduction to Python", 
 "Learn the basics of Python programming, including syntax, data types, and how to create simple applications. This course is perfect for beginners with no prior coding experience.",
-"['lesson_00001','quiz_00001','lesson_00002','quiz_00002','notes_00001']"),
+"['lesson_00001','quiz_00001','lesson_00002','quiz_00002','notes_00001']",
+10.00),
 ("course_00002", "Learning Mandarin", 
 " 你好! Learn the basics of Mandarin, including pronunciation, simple phrases, and everyday vocabulary. Perfect for travelers and language enthusiasts.",
-"['lesson_00010','quiz_00011','lesson_00012','quiz_00013','notes_00014']")
+"['lesson_00010','quiz_00011','lesson_00012','quiz_00013','notes_00014']",
+15.00)
 
 
