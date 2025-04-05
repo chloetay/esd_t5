aca7d8fe93b53d8a472ec823681abf0d48a36ebe
chloetay <chloe.tay.2023@scis.smu.edu.sg>
2025-04-05 15:16:26 +0800

    edited stuff config for my enroll composite

--- backend/Course/db/init.sql
+++ backend/Course/db/init.sql
@@ -1,7 +1,9 @@
 
+DROP TABLE IF EXISTS course;
 DROP TABLE IF EXISTS COURSE;
+ 
 
-CREATE TABLE COURSE (
+CREATE TABLE course (
     courseId VARCHAR(255) PRIMARY KEY,
     courseName VARCHAR(255) NOT NULL,
     courseDescription VARCHAR(255),
@@ -9,7 +11,7 @@ CREATE TABLE COURSE (
     courseCost DECIMAL(10,2) DEFAULT 0.00
 );
 
-INSERT INTO COURSE (courseId, courseName, courseDescription, courseContent, courseCost) VALUES
+INSERT INTO course (courseId, courseName, courseDescription, courseContent, courseCost) VALUES
 ('course_00001', 'Introduction to Python',
  'Learn the basics of Python programming, including syntax, data types, and how to create simple applications.',
  "['lesson_00001','quiz_00001','lesson_00002','quiz_00002','notes_00001']", 19.99),
